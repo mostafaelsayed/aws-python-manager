98ee8b550d76c10ceeaf976b8bc5c19415536cf8
mostafaelsayed <mostafaelsayed9419@gmail.com>
2025-02-18 20:46:08 +0200

    check for volumes requirement first before creating ecs task

--- ecs/ecs_create_task_definition.py
+++ ecs/ecs_create_task_definition.py
@@ -7,14 +7,16 @@ region_value = client.get_region()
 env = client.get_env()
 reference = parse('ecs/task-configuration/current.yaml')['task_reference']
 task_definition = parse('ecs/task-configuration/{0}/task-definition.yaml'.format(reference))
-task_definition_volumes = task_definition['volumes']
+
 volumes = []
-for volume in task_definition_volumes:
-    efs_name = volume['efsVolumeConfiguration']['fileSystemName']
-    file_system_id = efs_utils.get_file_system_id_by_name(efs_name)
-    volume['efsVolumeConfiguration']['fileSystemId'] = file_system_id
-    del volume['efsVolumeConfiguration']['fileSystemName']
-    volumes.append(volume)
+if 'volumes' in task_definition:
+    task_definition_volumes = task_definition['volumes']
+    for volume in task_definition_volumes:
+        efs_name = volume['efsVolumeConfiguration']['fileSystemName']
+        file_system_id = efs_utils.get_file_system_id_by_name(efs_name)
+        volume['efsVolumeConfiguration']['fileSystemId'] = file_system_id
+        del volume['efsVolumeConfiguration']['fileSystemName']
+        volumes.append(volume)
 response = instance.register_task_definition(
     volumes=volumes,
     containerDefinitions=task_definition['containerDefinitions'],
