fcff26a62172dd4449190da51604624417ce849f
mostafaelsayed <mostafaelsayed9419@gmail.com>
2025-12-11 00:10:20 +0200

    change client init

--- available_services.py
+++ available_services.py
@@ -1,6 +1,7 @@
-import boto3
 import utils.file_utils as file_utils
 
-available_services = boto3._get_default_session().get_available_services()
+from clients.default_client import DefaultClient
+
+available_services = DefaultClient().get_session().get_available_services()
 
 file_utils.write_json(available_services, 'output/available-services.json')
--- clients/default_client.py
+++ clients/default_client.py
@@ -1,20 +1,7 @@
-from config.config import load_default
 import boto3
 
 class DefaultClient:
-    def __init__(self) -> None:
-        self.credentials, self.region_value, self.env = load_default()
-
-    def get_credentials(self):
-        return self.credentials
-    
-    def get_region(self):
-        return self.region_value
-    
-    def get_env(self):
-        return self.env
-
+    def __init__(self):
+        self.sess = boto3.Session(profile_name='oidc')
     def get_instance(self, service_name):
-        return boto3.client(service_name, region_name=self.region_value,
-                            aws_access_key_id=self.credentials['aws_access_key_id'],
-                            aws_secret_access_key=self.credentials['aws_secret_access_key'])
+        return self.sess.client(service_name)
